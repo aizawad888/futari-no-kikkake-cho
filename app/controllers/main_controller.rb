3ec1d529ead6f46473288b3cfafd25b15d92e4c5
aizawad888 <201208476+aizawad888@users.noreply.github.com>
2025-12-12 16:33:35 +0900

    feat:#26_答え合わせ時間による投稿タイトルの分岐、答え合わせ時間のカラースタイル設定

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -7,6 +7,6 @@ class MainController < ApplicationController
     @partner = @pair.user1 == current_user ? @pair.user2 : @pair.user1
 
     # 自分と相手の投稿をまとめて取得（最新順）
-    @posts = Post.where(user: [current_user, @partner]).order(created_at: :desc)
+    @posts = Post.where(user: [ current_user, @partner ]).order(created_at: :desc)
   end
 end
--- db/migrate/20251212053714_create_pairs.rb
+++ db/migrate/20251212053714_create_pairs.rb
@@ -7,6 +7,6 @@ class CreatePairs < ActiveRecord::Migration[7.2]
       t.timestamps
     end
 
-    add_index :pairs, [:user1_id, :user2_id], unique: true
+    add_index :pairs, [ :user1_id, :user2_id ], unique: true
   end
 end
