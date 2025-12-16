c0c19329de29655834b301e07dda6d827f61dee9
aizawad888 <201208476+aizawad888@users.noreply.github.com>
2025-12-16 13:07:20 +0900

    feat:#28_#29_投稿の編集と削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_post!, only: [:edit, :update, :destroy]
+
   def new
     @post = Post.new
   end
@@ -17,6 +20,33 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to main_path, notice: "削除しました"
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def authorize_post!
+    return if @post.user == current_user
+
+    redirect_to main_path, alert: "権限がありません"
+  end
 
   private
 
